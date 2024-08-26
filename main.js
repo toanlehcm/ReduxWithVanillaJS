43e2c0b7aed4dd03e9722145327670f2b156ce2c
Toan Le <thanhtoanhcm1602@gmail.com>
2024-08-26 15:13:59 +0700

    - Handle submit form.

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-console.log(window.Redux);
+// console.log(window.Redux);
 
 const { createStore } = window.Redux
 
 // SETUP REDUX STORE.
 
 // State.
-const initialState = [
-  'Listen to music',
-];
+const initialState = JSON.parse(localStorage.getItem('hobby_list')) || [];
 
 // Reducer.
 const hobbyReducer = (state = initialState, action) => {
-  return state
+  switch (action.type) {
+    case 'ADD_HOBBY':
+      const newList = [...state];
+      newList.push(action.payload);
+      return newList;
+
+    default:
+      return state
+  }
 }
 
 // Store: có thể nhận 1 hoặc nhiều reducer.
@@ -39,3 +45,42 @@ const renderHobbyList = (hobbyList) => {
 // store lúc này chỉ có 1 reducer là hobbyReducer, trong hobbyReducer chỉ có 1 state là initialState, nên có thể gọi trực tiếp getState để lấy state.
 const initialHobbyList = store.getState();
 renderHobbyList(initialHobbyList)
+
+// HANDLE FORM SUBMIT.
+const hobbyFormEle = document.querySelector('#hobbyFormId');
+
+if (hobbyFormEle) {
+  const handleFormSubmit = (e) => {
+    // Prevent browser from reloading.
+    e.preventDefault();
+
+    const hobbyTextEle = hobbyFormEle.querySelector('#hobbyTextId');
+    if (!hobbyTextEle) return;
+
+
+    // console.log('submit', hobbyTextEle.value);
+
+    // Create action.
+    const action = {
+      type: 'ADD_HOBBY',
+      payload: hobbyTextEle.value
+    }
+
+    // Dispatch action to reducer.
+    store.dispatch(action)
+
+    // Reset form.
+    hobbyTextEle.value = '';
+    hobbyFormEle.reset();
+  }
+
+  hobbyFormEle.addEventListener('submit', handleFormSubmit); // When typing Enter.
+}
+
+store.subscribe(() => {
+  // Get new state.
+  const hobbyList = store.getState();
+  renderHobbyList(hobbyList);
+
+  localStorage.setItem('hobby_list', JSON.stringify(hobbyList));
+})
